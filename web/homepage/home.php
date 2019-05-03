99345c313b8d7f39043c8110b45d33c6403a0679
Cameron Smith <ca.smith657@gmail.com>
2019-05-03 12:09:09 -0600

    created a homepage

---
+++ web/homepage/home.php
@@ -0,0 +1,42 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+	<meta charset="utf-8"/>
+	<script type="text/javascript" src = "cool.js"></script>
+	<link rel = "stylesheet" type = "text/css" href = "styles.css">
+	<title>Cameron Smith CS313</title>
+</head>
+<body>
+	<div id = "header">
+		<h1>Cameron Smith's CS313 Home Page</h1>
+		<a href = "home.php" class = "links">Home Page &nbsp; </a>
+		<a href = "assignments.html" class = "links">Assignment Directory</a> 
+	</div>
+	<div id = "content">
+		<p> Here you can find all my assignments for CS313: Web deveopment 2. I am a student at BYUI
+			and am a senior this year. I hope to graduate this year and continue onward into employment. 
+			In terms of hobbies I enjoy playing competitive tekken and I love to listen to and play music.
+			I play both guitar and bass (see pictures). I dream that one day I am skilled enough to write
+		music and bring my own ideas into the world.</p>
+		<br>
+		<br>
+		<div class = "row">
+			<div class = "column">
+				<div class = "img-wtih-text">
+					<a href = "http://steamcommunity.com/id/fightingym/"><img src = "comboreel.gif" id = "gifs"></a>
+					<p> I'm always up for a challenge. Add me and let's fight!</p>
+				</div>
+			</div>
+			<div class = "column">
+				<div class = "img-with-text">
+					<a href = "https://open.spotify.com/user/rottensoup/playlist/17XaadAcOg1NvyTzV65BUQ?si=ixK-z8HcTsi4o_NTz-Mhvw"><img src = "canvas.png" id = "gifs"></a>
+					<p> Looking to jam? Check out my playlist!</p>
+				</div>
+			</div>
+		</div>
+		<br>
+		<br>
+		
+	</div>
+</body>
+</html>
