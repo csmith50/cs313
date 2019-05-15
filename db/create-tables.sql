c6f817016f0e9b0b16f982157795968e99d4dd21
Cameron Smith <ca.smith657@gmail.com>
2019-05-15 11:22:01 -0600

    code to create database added

---
+++ db/create-tables.sql
@@ -0,0 +1,29 @@
+CREATE TABLE public.finances
+{
+	id SERIAL NOT NULL PRIMARY KEY,
+	card! INT,
+	card2 INT,
+	card3 INT,
+	paypal VARCHAR(100)	
+};
+
+CREATE TABLE public.user
+{
+	id SERIAL NOT NULL PRIMARY KEY,
+	username VARCHAR(20) NOT NULL UNIQUE,
+	password VARCHAR(100) NOT NULL,
+	description VARCHAR(500),
+	paymentInfoID INT NOT NULL REFERENCES public.finances(id)
+};
+
+CREATE TABLE public.itemsList
+{
+	id SERIAL NOT NULL PRIMARY KEY,
+	namme VARCHAR(250) NOT NULL,
+	currentBid NUMERIC NOT NULL,
+	description VARCHAR(500),
+	currentBidUser INT NOT NULL REFERENCES public.user(id),
+	photoName VARCHAR(50),
+	condition VARCHAR(50),
+	owner INT NOT NULL REFERENCES public.user(id)
+};
