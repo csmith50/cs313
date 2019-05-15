8ca14ccf2a81b104797af9870479767a57804912
Cameron Smith <ca.smith657@gmail.com>
2019-05-15 12:01:56 -0600

    made some changes

--- db/create-tables.sql
+++ db/create-tables.sql
@@ -1,23 +1,23 @@
 CREATE TABLE public.finances
-{
+(
 	id SERIAL NOT NULL PRIMARY KEY,
-	card! INT,
+	card1 INT,
 	card2 INT,
 	card3 INT,
 	paypal VARCHAR(100)	
-};
+);
 
 CREATE TABLE public.user
-{
+(
 	id SERIAL NOT NULL PRIMARY KEY,
 	username VARCHAR(20) NOT NULL UNIQUE,
 	password VARCHAR(100) NOT NULL,
 	description VARCHAR(500),
-	paymentInfoID INT NOT NULL REFERENCES public.finances(id)
-};
+	paymentInfoID INT REFERENCES public.finances(id)
+);
 
 CREATE TABLE public.itemsList
-{
+(
 	id SERIAL NOT NULL PRIMARY KEY,
 	namme VARCHAR(250) NOT NULL,
 	currentBid NUMERIC NOT NULL,
@@ -25,5 +25,6 @@ CREATE TABLE public.itemsList
 	currentBidUser INT NOT NULL REFERENCES public.user(id),
 	photoName VARCHAR(50),
 	condition VARCHAR(50),
-	owner INT NOT NULL REFERENCES public.user(id)
-};
+	owner INT NOT NULL REFERENCES public.user(id),
+	datePosted TIMESTAMPTZ NOT NULL
+);
