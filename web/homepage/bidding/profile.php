8533a15a6d34c4aa9ce8d45ef5a12cbfd26c3a1b
Cameron Smith <ca.smith657@gmail.com>
2019-06-05 11:38:21 -0600

    sytle fixes

--- web/homepage/bidding/profile.php
+++ web/homepage/bidding/profile.php
@@ -34,55 +34,56 @@ $db = get_db();
 	</div>
 
 
-	<div class = "center">
-		<?php
-			if (!empty($_GET['id']) && $_GET['id'] == $_SESSION['loginID']) {
-          			echo "<a href = \"https://vast-crag-32349.herokuapp.com/homepage/bidding/editProfile.php\">edit profile</a>";
-          		}
+	<div>
+		<div class = "center">
+			<?php
+				if (!empty($_GET['id']) && $_GET['id'] == $_SESSION['loginID']) {
+	          			echo "<a href = \"https://vast-crag-32349.herokuapp.com/homepage/bidding/editProfile.php\">edit profile</a>";
+	          		}
 
-			if (!empty($_GET['id']) && $_GET['id'] == $_SESSION['loginID'])
-				$login = $_SESSION['loginID'];
-			else if (!empty($_GET['id']) && $_GET['id'] != $_SESSION['loginID'])
-				$login = $_GET['id'];
-			else
-				$login = $_SESSION['loginID'];
+				if (!empty($_GET['id']) && $_GET['id'] == $_SESSION['loginID'])
+					$login = $_SESSION['loginID'];
+				else if (!empty($_GET['id']) && $_GET['id'] != $_SESSION['loginID'])
+					$login = $_GET['id'];
+				else
+					$login = $_SESSION['loginID'];
 
-			$statement = $db->prepare("SELECT username, description FROM public.user WHERE id = $login");
-			$statement->execute();
-			$row = $statement->fetch(PDO::FETCH_ASSOC);
-			$username = $row['username'];
-			$desc = $row['description'];
-			echo "<h1>$username</h1><br>";
-			echo "<p>$desc</p><br>";
+				$statement = $db->prepare("SELECT username, description FROM public.user WHERE id = $login");
+				$statement->execute();
+				$row = $statement->fetch(PDO::FETCH_ASSOC);
+				$username = $row['username'];
+				$desc = $row['description'];
+				echo "<h1>$username</h1><br>";
+				echo "<p>$desc</p><br>";
 
-			$db = get_db();
-			$statement = $db->prepare("SELECT  id, name, photoname FROM itemsList WHERE owner = $login");
-			$statement->execute();
-			echo "<h3>items this user is selling</h3><br>";
-			echo "</div>";
+				$db = get_db();
+				$statement = $db->prepare("SELECT  id, name, photoname FROM itemsList WHERE owner = $login");
+				$statement->execute();
+				echo "<h3>items this user is selling</h3><br>";
+				echo "</div>";
 
-			$isLeft = true;
-			foreach ($statement as $row) {
-  				$name = $row['name'];
-  				$photoName = $row['photoname'];
-  				$id = $row['id'];
+				$isLeft = true;
+				foreach ($statement as $row) {
+	  				$name = $row['name'];
+	  				$photoName = $row['photoname'];
+	  				$id = $row['id'];
 
-  				if ($isLeft) 
-  					echo "<div class = \"itemLeft\">";
-  				else 
-  					echo "<div class = \"itemRight\">";
+	  				if ($isLeft) 
+	  					echo "<div class = \"itemLeft\">";
+	  				else 
+	  					echo "<div class = \"itemRight\">";
 
-  				echo "<img src = \"";
-          		echo $photoName;
-          		echo "\" height = \"200px\" width = \"200px\" alt = \"missing photo\">";
-  				echo "<a href = \"https://vast-crag-32349.herokuapp.com/homepage/bidding/itemPage.php?name=$name&photoName=$photoName&id=$id\">$name</a>";
-  				echo "</div>";
+	  				echo "<img src = \"";
+	          		echo $photoName;
+	          		echo "\" height = \"200px\" width = \"200px\" alt = \"missing photo\">";
+	  				echo "<a href = \"https://vast-crag-32349.herokuapp.com/homepage/bidding/itemPage.php?name=$name&photoName=$photoName&id=$id\">$name</a>";
+	  				echo "</div>";
 
-          		if (!$isLeft)
-            		echo "<br><Br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"; //line break on right item
-          		$isLeft = !$isLeft;
-  			}
-		?>
-	<!--</div>-->
+	          		if (!$isLeft)
+	            		echo "<br><Br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br><br>"; //line break on right item
+	          		$isLeft = !$isLeft;
+	  			}
+			?>
+	</div>
 </body>
 </html>
